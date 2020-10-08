668efd458425bfc1f47a989c5d448619f58bdc65
Alexander Repeta <alexander.repeta@gmail.com>
2020-10-08 20:26:54 +0300

    Добавил исходники

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const exhbs = require('express-handlebars');
+const products = require('./products.json');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exhbs({
+    extname: 'hbs',
+  }),
+);
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { cssFileName: 'about', pageTitle: 'О нас' });
+});
+
+app.get('/products', (req, res) => {
+  res.render('products', {
+    products,
+    cssFileName: 'products',
+    pageTitle: 'Наши продукты',
+  });
+});
+
+app.get('/product/:productId', (req, res) => {
+  console.log(req.params);
+
+  const product = products.find(p => p.id === req.params.productId);
+
+  res.render('product', { product });
+});
+
+app.listen(4444, () => {
+  console.log(`Application server is running on port ${4444}`);
+});
